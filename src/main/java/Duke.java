04720aa68a2d933699340b04d00b40abe776a982
MUTHUKUMAR <imuthukumar1998@gmail.com>
2020-08-15 14:40:45 +0800

    final edit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,10 @@
 
 public class Duke{
     public static void main(String[] args) {
-/**
- * Initial code to test out this file
- *       String logo = " ____        _        \n"
- *               + "|  _ \\ _   _| | _____ \n"
- *               + "| | | | | | | |/ / _ \\\n"
- *               + "| |_| | |_| |   <  __/\n"
- *               + "|____/ \\__,_|_|\\_\\___|\n";
- *       System.out.println("Hello from\n" + logo);
- *
- */
-    System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-    System.out.println("Bye.Hope to see you soon!");
+        System.out.println("-------------------------------");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        System.out.println("-------------------------------");
+        System.out.println("Bye.Hope to see you soon!");
+        System.out.println("-------------------------------");
     }
 }
