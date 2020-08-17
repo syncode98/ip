8c50b05dfb45eb325bfe9899c39a388d635e560d
MUTHUKUMAR <imuthukumar1998@gmail.com>
2020-08-17 13:28:51 +0800

    clean code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 
 public class Duke{
     public static void main(String[] args) {
+        
         System.out.println("-------------------------------");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
         System.out.println("-------------------------------");
