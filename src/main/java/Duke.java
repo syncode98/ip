ddaf38966e489b25936a9f596bb575586091ca28
MUTHUKUMAR <imuthukumar1998@gmail.com>
2020-08-19 23:14:52 +0800

    Implementing level-0 for the code
    The Duke symbol was removed from the code.
    This symbol was used to test out the code when it was cloned from
    a remote directory.
    
    Two statements and a few lines were added.These act as a base
    for future communications.
    
    This the is required task for week 2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,12 @@
 
 public class Duke{
     public static void main(String[] args) {
-        
+
         System.out.println("-------------------------------");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
         System.out.println("-------------------------------");
         System.out.println("Bye.Hope to see you soon!");
         System.out.println("-------------------------------");
+
     }
 }
