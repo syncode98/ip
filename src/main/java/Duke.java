3a5b25a77ecd3361c14489daa22202b9689e885a
IMUTHUKUMAR <imuthukumar1998@gmail.com>
2020-08-24 18:57:17 +0800

    Implement Level 1 for Chatbot
    Chatbot echo's user's commands and exits when
    the user types "bye".
    
    Function printLines prints dotted lines. This improves
     code readability.
    
    Users would be able to enter their name and Chatbot
    records it. Addresses the user with the name
    given.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
 
 public class Duke{
-    public static void main(String[] args) {
 
+    public static void printLines(){
         System.out.println("-------------------------------");
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-        System.out.println("-------------------------------");
-        System.out.println("Bye.Hope to see you soon!");
-        System.out.println("-------------------------------");
+    }
+    public static void main(String[] args) {
+        Scanner inputScanner= new Scanner(System.in);
+        String input;
+        printLines();
+        System.out.println("Hello! I'm Mike!\nMay I have your name please?");
+        printLines();
+        String name= inputScanner.nextLine();
+        printLines();
+        System.out.println("Alright " +name+ " , What can I do for you?");
+        printLines();
+        input=inputScanner.nextLine();
+        while(!input.equals("bye")){
+            printLines();
+            System.out.println(input);
+            printLines();
+            input=inputScanner.nextLine();
+        }
+
+        printLines();
+        System.out.println("Bye.Hope to see you again soon!");
+        printLines();
 
     }
 }
