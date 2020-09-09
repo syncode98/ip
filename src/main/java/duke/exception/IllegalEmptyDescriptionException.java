96aef3d2ca143496b8a5270ff3b846aa6739d9d1
IMUTHUKUMAR <imuthukumar1998@gmail.com>
2020-09-09 23:49:55 +0800

    Implementing exceptions
    Three exception classes have been defined to
    tackle the exceptions that arise.
    
    Try and catch functions have been defined to
     deal with exceptions that would arise in methods.

--- src/main/java/duke/exception/IllegalEmptyDescriptionException.java
+++ src/main/java/duke/exception/IllegalEmptyDescriptionException.java
@@ -1,8 +1,9 @@
 package duke.exception;
 
+/**
+ * To be called upon if user inputs invalid task description for all types of task objects.
+ */
 public class IllegalEmptyDescriptionException extends Exception {
 
-    /**
-     * To be called upon if user inputs invalid task description for all types of task objects.
-     */
+
 }
--- src/main/java/duke/exception/IllegalPrepositionWithoutDate.java
+++ src/main/java/duke/exception/IllegalPrepositionWithoutDate.java
@@ -1,7 +1,7 @@
 package duke.exception;
 
 /**
- * To be called upon if user event/deadline with preposition , but without a date
+ * To be called upon if user enters an event/deadline with preposition , but without a date
  */
 public class IllegalPrepositionWithoutDate extends Exception {
 
