e37a51ae861caf7a3ce4cf868646ea2b353e8558
IMUTHUKUMAR <imuthukumar1998@gmail.com>
2020-09-22 00:09:16 +0800

    Code refactor for TaskList.java
    The class now takes care of the relevant methods
    for the taskArrayList.

---
+++ src/main/java/duke/TaskList.java
@@ -0,0 +1,54 @@
+package duke;
+
+import duke.task.Task;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static duke.Storage.writeToFile;
+
+public class TaskList {
+    public static final int MAX_SIZE = 100;
+    public static ArrayList<Task> taskArrayList = new ArrayList<>();
+
+    public static String DELIMITER_EMPTY_STRING = "";
+    public static String DELIMITER_SLASH = "/";
+    public static String DELIMITER_SEMI_COLON = ":";
+    public static String DELIMITER_CHARACTER = " ";
+    public static String DIVIDER = " | ";
+
+    public TaskList() {
+        taskArrayList = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> dataTexts) {
+        taskArrayList = dataTexts;
+
+    }
+
+    public static int getSize() {
+        return taskArrayList.size();
+    }
+
+    public static void addToTasks(Task task, boolean fileWrite) throws IOException {
+        taskArrayList.add(task);
+        task.addTask();
+        if (fileWrite) {
+            writeToFile(task.toString());
+            System.out.println("Now you have " + getSize() + " tasks in the list.");
+        }
+    }
+
+
+    public static void printTasks() {
+        Ui.printAllTasks(taskArrayList);
+    }
+
+    public static void removeTask(Task task) {
+        taskArrayList.remove(task);
+    }
+}
+
+
+
+
