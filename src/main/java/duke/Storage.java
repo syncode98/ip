8cbee6243bc512dd6815aadd20736958a5a24ab6
IMUTHUKUMAR <imuthukumar1998@gmail.com>
2020-09-24 22:34:43 +0800

    Code refactoring

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -16,7 +16,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static duke.command.AddCommand.returnTask;
+import static duke.AddCommand.returnTask;
+
 
 public class Storage {
 
