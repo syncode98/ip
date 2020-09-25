6ff9d47a8d1da4c2e79bcfe153c75df01bea67a2
IMUTHUKUMAR <imuthukumar1998@gmail.com>
2020-09-25 20:09:44 +0800

    Implement time and date features in Event class
    The toString() method converts the date and time
    of the event to string. It is appended to the task
    to give the final output.

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,28 +1,78 @@
 package duke.task;
 
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Event extends Task {
-    private String startAndEndTime;
+
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
 
     public Event(String task, boolean isDone, String startAndEndTime) {
         super(task, isDone);
-        this.startAndEndTime = startAndEndTime;
+
+
     }
 
     public Event(String task, String startAndEndTime) {
         super(task, false);
-        this.startAndEndTime = startAndEndTime;
+
     }
 
-    public String getStartAndEndTime() {
-        return startAndEndTime;
+    public Event(String task, LocalDate date, LocalTime startTime, LocalTime endTime) {
+        super(task, false);
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+
     }
 
-    public void setStartAndEndTime(String startAndEndTime) {
-        this.startAndEndTime = startAndEndTime;
+    public LocalDate getDate() {
+        return date;
     }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(" + this.startAndEndTime + ")";
+        String date_format;
+        String startAndEndTime = "";
+
+        if (this.startTime != null) {
+            startAndEndTime += "at: " + this.startTime.format(DateTimeFormatter.ofPattern("HH mm"));
+        }
+        if (this.endTime != null) {
+            startAndEndTime += "-" + this.endTime.format(DateTimeFormatter.ofPattern("HH mm"));
+        }
+        if (this.date != null) {
+            date_format = date.format(DateTimeFormatter.ofPattern("MMM d yyyy "));
+            startAndEndTime += " " + date_format;
+        }
+
+        return "[E]" + super.toString() + "(" + startAndEndTime + ")";
     }
 }
