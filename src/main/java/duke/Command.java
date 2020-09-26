8343c43d7eec25a6ee9619a4c02bab2d63909c41
IMUTHUKUMAR <imuthukumar1998@gmail.com>
2020-09-26 14:58:08 +0800

    Add JavaDoc for Command.java class and Ui.java class

--- src/main/java/duke/Command.java
+++ src/main/java/duke/Command.java
@@ -5,6 +5,15 @@ import duke.exception.IllegalNumberException;
 public abstract class Command {
     public String command;
 
+    /**
+     * Checks if the user is finding for a task that is
+     * present in the taskArrayList.
+     *
+     * @param command The number entered by the user
+     * @return The index of the task.
+     * @throws IllegalNumberException If the number represents a task that
+     *                                is not present in the taskArrayList
+     */
     public static int findTaskNumber(String command) throws IllegalNumberException {
 
         int indexOfTask;
